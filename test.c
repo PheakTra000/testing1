7112bfc2e515861314bf5183ef895d949b645758
tra <tralegendary99@gmail.com>
2025-08-05 11:23:34 +0700

    jeff

--- test.c
+++ test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello world!!\n This is a new user for git!!");
+    int size;
+    printf("Enter the number of element: ");
+    scanf("%d", &size);
 
+    int arr[size];
+    printf("Add %d element", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
 }
